4c877fa2486b2daac3e727aa93bcd7bba12e59d9
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-03-05 23:37:00 -0300

    feat: add hover behaviour to Widget

--- pudu_ui/widget.py
+++ pudu_ui/widget.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pyglet
 
 
 @dataclass
@@ -7,7 +8,7 @@ class Params:
     y: float = 0.0
     width: int = 100
     height: int = 100
-    focusable: bool = False
+    focusable: bool = True
 
 
 class Widget:
@@ -19,6 +20,7 @@ class Widget:
         self._visible: bool = True
         self.focusable: bool = params.focusable
         self.is_on_focus: bool = False
+        self.is_on_hover: bool = False
         self.index: int = 0
         self.parent: Widget = parent
         self.is_valid: bool = True
@@ -29,6 +31,9 @@ class Widget:
     def on_unfocus(self):
         pass
 
+    def on_hover(self):
+        pass
+
     def focus(self):
         if self.focusable:
             self.is_on_focus = True
@@ -37,7 +42,13 @@ class Widget:
     def unfocus(self):
         if self.focusable:
             self.is_on_focus = False
+            self.is_on_hover = False
             self.on_unfocus()
+
+    def hover(self):
+        if self.focusable:
+            self.is_on_hover = True
+            self.on_hover()
 
     def invalidate(self):
         self.is_valid = False
@@ -55,6 +66,15 @@ class Widget:
             (self.x <= x <= self.x + self.width) and
             (self.y <= y <= self.y + self.height)
         )
+
+    def on_mouse_motion(self, x, y, dx, dy) -> bool:
+        if self.is_inside(x, y):
+            if not self.is_on_hover:
+                self.hover()
+        else:
+            if self.is_on_hover and not self.is_on_focus:
+                self.unfocus()
+        return pyglet.event.EVENT_UNHANDLED
 
     def __repr__(self) -> str:
         return f"Widget(x: {self.x}, y: {self.y}. width: {self.width}, height:"\
