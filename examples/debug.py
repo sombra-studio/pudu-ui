325deae7f0604c36eec808a757deda311a204722
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-03-05 23:45:50 -0300

    feat: add hover to Frame

--- examples/debug.py
+++ examples/debug.py
@@ -3,16 +3,8 @@ import pyglet
 
 
 window = pyglet.window.Window()
-# window.projection = pyglet.math.Mat4.orthogonal_projection(
-#     0, window.width, 0, window.height, 0.1, 100
-# )
 batch = pyglet.graphics.Batch()
-# quad = pudu_ui.primitives.quad.RoundedQuad(
-#     x=50, y=100,
-#     width=300,
-#     height=120,
-#     batch=batch
-# )
+
 params = pudu_ui.primitives.FrameParams(
     x=50, y=100,
     width=300,
--- pudu_ui/primitives/frame.py
+++ pudu_ui/primitives/frame.py
@@ -42,6 +42,8 @@ class FrameParams(Params):
     radius_bottom_left: float = 0
     radius_bottom_right: float = 0
     gradient_direction: GradientDirection = GradientDirection.VERTICAL
+    # ATTENTION: By default Frame won't be focusable
+    focusable: bool = False
 
 
 class Frame(Widget):
@@ -49,27 +51,24 @@ class Frame(Widget):
         self, params: FrameParams, batch: Batch = None, group: Group = None
     ):
         super().__init__(params)
-        if params.color_type == ColorType.SOLID:
-            colors = (
-                params.background_color, params.background_color,
-                params.background_color, params.background_color
-            )
-        else:
-            colors = (
-                params.background_gradient.end_color,
-                params.background_gradient.end_color,
-                params.background_gradient.start_color,
-                params.background_gradient.start_color
-            )
-        if params.gradient_direction == GradientDirection.HORIZONTAL:
-            # this would be like swapping v0 with v2 colors
-            colors = (colors[2], colors[1], colors[0], colors[3])
-        self.background = pudu_ui.primitives.RoundedQuad(
+        self.color_type = params.color_type
+        self.gradient_direction = params.gradient_direction
+        # set background, focus and hover colors
+        self.background_colors = self.get_colors(
+            params.background_color, params.background_gradient
+        )
+        self.focus_colors = self.get_colors(
+            params.focus_color, params.focus_gradient
+        )
+        self.hover_colors = self.get_colors(
+            params.hover_color, params.hover_gradient
+        )
+        self.quad = pudu_ui.primitives.RoundedQuad(
             params.x,
             params.y,
             params.width,
             params.height,
-            colors=colors,
+            colors=self.background_colors,
             opacity=params.opacity,
             radius_top_left=params.radius_top_left,
             radius_top_right=params.radius_top_right,
@@ -78,3 +77,38 @@ class Frame(Widget):
             batch=batch,
             group=group
         )
+
+    def get_colors(
+        self, color: Color, gradient: ColorGradient
+    ):
+        if self.color_type == ColorType.SOLID:
+            colors = (
+                color, color,
+                color, color
+            )
+        else:
+            colors = (
+                gradient.end_color,
+                gradient.end_color,
+                gradient.start_color,
+                gradient.start_color
+            )
+        if self.gradient_direction == GradientDirection.HORIZONTAL:
+            # this would be like swapping v0 with v2 colors
+            colors = (colors[2], colors[1], colors[0], colors[3])
+        return colors
+
+    def change_quad_colors(self, colors: tuple[Color, Color, Color, Color]):
+        self.quad.colors = colors
+        self.quad.set_data()
+        # data comes like a tuple with (format, data_list)
+        # data['vertex_color'] will be like ('Bn', [100, 23, 90, 120, 80, ...])
+        # so we only care about the second part here
+        new_colors = self.quad.data['vertex_color'][1]
+        self.quad.vertex_list.vertex_color[:] = new_colors
+
+    def on_unfocus(self):
+        self.change_quad_colors(self.background_colors)
+
+    def on_hover(self):
+        self.change_quad_colors(self.hover_colors)
