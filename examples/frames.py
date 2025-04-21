a673c9c122f7db468482f29aafbffb06a25366be
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-04-21 00:14:58 -0400

    docs: update example to new Frame location

--- examples/frames.py
+++ examples/frames.py
@@ -5,23 +5,23 @@ import pyglet
 window = pyglet.window.Window()
 batch = pyglet.graphics.Batch()
 
-params = pudu_ui.primitives.FrameParams(
+params = pudu_ui.FrameParams(
     x=50, y=100,
     width=300,
     height=120
 )
-frames = [pudu_ui.primitives.Frame(params, batch=batch)]
+frames = [pudu_ui.Frame(params, batch=batch)]
 
 params.x += 400
 params.style.end_color = pudu_ui.colors.DARK_PURPLE
-frames.append(pudu_ui.primitives.Frame(params, batch=batch))
+frames.append(pudu_ui.Frame(params, batch=batch))
 
 params.y += 200
 params.style.radius_top_left = 24
 params.style.radius_bottom_left = 24
 params.style.start_color = pudu_ui.colors.MEDIUM_BLUE
 params.style.end_color = pudu_ui.colors.MEDIUM_BLUE
-frames.append(pudu_ui.primitives.Frame(params, batch=batch))
+frames.append(pudu_ui.Frame(params, batch=batch))
 
 params.x -= 400
 params.style.radius_top_right = 24
@@ -29,7 +29,7 @@ params.style.radius_bottom_right = 24
 params.style.gradient_direction = pudu_ui.colors.GradientDirection.HORIZONTAL
 params.style.start_color = pudu_ui.colors.LIGHT_BLUE_GREEN
 params.style.end_color = pudu_ui.colors.MEDIUM_BLUE
-frames.append(pudu_ui.primitives.Frame(params, batch=batch))
+frames.append(pudu_ui.Frame(params, batch=batch))
 
 
 @window.event
