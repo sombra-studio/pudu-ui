3ece45b97d56e43744c83243aa3d3eb6bc5e75cc
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-07-03 19:02:11 -0400

    wip: grid in calculator

--- examples/calculator.py
+++ examples/calculator.py
@@ -1,50 +1,58 @@
 from pudu_ui import (
-    App, Button, ButtonParams, Frame, FrameParams, Label, LabelParams
+    App, Button, ButtonParams
 )
-from pudu_ui.layouts import GridLayout, GridLayoutParams
-import pudu_ui
-import pyglet
+from pudu_ui.layouts import (
+    GridLayout, GridLayoutParams, ListLayout,
+    ListLayoutParams
+)
 
-
-WIDTH = 800
+SYMBOLS = [
+    ["1", "2", "3", "+"],
+    ["4", "5", "6", "-"],
+    ["7", "8", "9", "x"],
+    [".", "0", "﹣", "/"],
+    ["C", "⌫", "="]
+]
+WIDTH = 350
 HEIGHT = 600
-BUTTON_SIZE = 40
+ITEM_GAP = 5
+M = len(SYMBOLS[0])
+N = len(SYMBOLS)
+BUTTON_SIZE = int(
+    round(
+        (WIDTH - (M + 1) * ITEM_GAP) / M
+    )
+)
 
 
 class Calculator(App):
     def __init__(self):
         super().__init__(width=WIDTH, height=HEIGHT, caption="Calculator")
-        symbols = [
-            ["1", "2", "3", "+"],
-            ["4", "5", "6", "-"],
-            ["7", "8", "9", "x"],
-            [".", "0", "﹣", "/"],
-            ["C", "⌫", "="]
-        ]
-        grid_width = len(symbols[0]) * BUTTON_SIZE
-        grid_height = (len(symbols) - 1) * BUTTON_SIZE
+        # Grid
+        grid_width = WIDTH
+        grid_height = grid_width
         params = GridLayoutParams(
-            x=100, y=20+10+BUTTON_SIZE, width=grid_width, height=grid_height,
+            x=0, y=0, width=grid_width, height=grid_height,
             rows=4, columns=4, item_gap=5
         )
         self.grid = GridLayout(params, batch=self.batch)
 
-        for row in symbols[:-1]:
+        for row in SYMBOLS[:-1]:
             for cell in row:
                 params = ButtonParams(text=cell)
-                params.set_uniform_radius(4)
+                params.set_uniform_radius(BUTTON_SIZE * 0.25)
                 button = Button(params, batch=self.batch)
                 self.push_handlers(button)
                 # button.set_debug_mode()
                 self.grid.add(button)
 
-        self.grid.set_debug_mode()
+        params = ListLayoutParams()
+        list_layout = ListLayout()
 
-    def update(self, dt):
-        self.grid.update(dt)
+        self.grid.set_debug_mode()
+        self.current_screen.widgets.append(self.grid)
 
 
 if __name__ == '__main__':
     calculator = Calculator()
-    pyglet.clock.schedule_interval(calculator.update, 1.0 / 60.0)
-    pyglet.app.run()
+    calculator.run()
