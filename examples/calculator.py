ee6fd2d215dc897f1b9b23f3c5a6b6ef5b38df2f
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-07-03 22:40:39 -0400

    wip: bottom list in calculator

--- examples/calculator.py
+++ examples/calculator.py
@@ -20,7 +20,7 @@ M = len(SYMBOLS[0])
 N = len(SYMBOLS)
 BUTTON_SIZE = int(
     round(
-        (WIDTH - (M + 1) * ITEM_GAP) / M
+        (WIDTH - 2 * M * ITEM_GAP) / M
     )
 )
 
@@ -28,29 +28,47 @@ BUTTON_SIZE = int(
 class Calculator(App):
     def __init__(self):
         super().__init__(width=WIDTH, height=HEIGHT, caption="Calculator")
+        # Container
+
+        # List
+        list_height = 2 * ITEM_GAP + BUTTON_SIZE
+        params = ListLayoutParams(
+            x=ITEM_GAP, y=ITEM_GAP, width=WIDTH, height=list_height,
+            item_height=BUTTON_SIZE, inter_item_spacing=(2 * ITEM_GAP),
+            resizes_item_width=False
+        )
+        list_layout = ListLayout(params, batch=self.batch)
+        for i, cell in enumerate(SYMBOLS[-1]):
+            button_size = BUTTON_SIZE
+            if i == (len(SYMBOLS[-1]) - 1):
+                button_size = 2 * BUTTON_SIZE
+            params = ButtonParams(width=button_size, text=cell)
+            params.set_uniform_radius(BUTTON_SIZE * 0.25)
+            button = Button(params, batch=self.batch)
+            list_layout.add(button)
+        list_layout.set_debug_mode()
+        self.current_screen.widgets.append(list_layout)
+
         # Grid
         grid_width = WIDTH
         grid_height = grid_width
         params = GridLayoutParams(
-            x=0, y=0, width=grid_width, height=grid_height,
-            rows=4, columns=4, item_gap=5
+            x=0, y=list_height, width=grid_width, height=grid_height,
+            rows=4, columns=4, item_gap=ITEM_GAP
         )
-        self.grid = GridLayout(params, batch=self.batch)
+        grid = GridLayout(params, batch=self.batch)
 
         for row in SYMBOLS[:-1]:
             for cell in row:
                 params = ButtonParams(text=cell)
                 params.set_uniform_radius(BUTTON_SIZE * 0.25)
                 button = Button(params, batch=self.batch)
-                self.push_handlers(button)
                 # button.set_debug_mode()
-                self.grid.add(button)
+                grid.add(button)
+        # grid.set_debug_mode()
+        self.current_screen.widgets.append(grid)
 
-        params = ListLayoutParams()
-        list_layout = ListLayout()
 
-        self.grid.set_debug_mode()
-        self.current_screen.widgets.append(self.grid)
 
 
 if __name__ == '__main__':
