f8e4b58aaa6632890ec0e15698a8d1da653c0b6d
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-02-19 17:30:24 -0300

    refactor: remove call to load on init of Controller to use it on Navigators

--- pudu_ui/controller.py
+++ pudu_ui/controller.py
@@ -5,6 +5,8 @@ from pudu_ui import App
 
 
 class State(Enum):
+    INITIALIZING = auto()
+    INITIALIZED = auto()
     LOADING = auto()
     ACTIVE = auto()
     PAUSED = auto()
@@ -15,24 +17,34 @@ class Controller:
     def __init__(
         self, app: App, name: str | None = None
     ):
+        self.state = State.INITIALIZING
         self.app: App = app
         if not name:
             name = self.__class__.__name__
         self.name: str = name
-        self.load()
+        self.state = State.INITIALIZED
 
     def load(self):
         self.on_load()
-        self.state = State.ACTIVE
+        self.resume()
 
     def pause(self):
         self.on_pause()
+
+    def resume(self):
+        self.on_resume()
 
     def close(self):
         self.on_close()
 
     def on_load(self):
         self.state = State.LOADING
+
+    def on_pause(self):
+        self.state = State.PAUSED
+
+    def on_resume(self):
+        self.state = State.ACTIVE
 
     def on_close(self):
         self.state = State.CLOSING
